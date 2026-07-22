9f5a49b6c3f8091c94e74b0a55f877442e41429c
venkatesh patil <vppatil819@gmail.com>
2026-07-22 10:11:38 +0530

    Add files via upload

---
+++ positive.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"the value of a is:";
+cin>>a;
+
+if(a>0){
+cout<<"a is positive";
+}
+
+else if (a==0){
+cout<<"a is zero";
+}
+
+else{
+cout<<"a is negative";
+}
+
+return 0;
+}
