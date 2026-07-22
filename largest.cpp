f74a585ab84b23c7cf8948f4b03a800952bfb67d
venkatesh patil <vppatil819@gmail.com>
2026-07-22 09:46:48 +0530

    Add files via upload

---
+++ largest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"the value of a is:";
+cin>>a;
+cout<<"the value of b is:";
+cin>>b;
+if (a>b){
+cout<<"a is greater";
+}
+else{
+cout<<"b is greater";
+}
+return 0;
+}
