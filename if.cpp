d56bc20af75fbe53a045fab7086b0695fe4d0360
venkatesh patil <vppatil819@gmail.com>
2026-07-22 09:36:23 +0530

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"value of a is:";
+cin>>a;
+cout<<endl;
+if(a%2==0){
+cout<<"a is even";
+}
+else{
+cout<<"a is odd";
+}
+return 0;
+}
