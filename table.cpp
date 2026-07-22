f388944b200bba71603169dfebedfa6287385359
venkatesh patil <vppatil819@gmail.com>
2026-07-22 11:02:49 +0530

    Add files via upload

---
+++ table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+cout<<"the value of n is:";
+cin>>n;
+
+for(int i=1; i<=10; i++){
+
+cout<<n<<"*"<<i<<"="<<n*i<<endl;
+}
+return 0;
+}
